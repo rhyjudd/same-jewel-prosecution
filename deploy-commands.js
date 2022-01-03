664dee1918d848c463d3ba4f130ecc35077dcfcf
Glitch (same-jewel-prosecution) <none>
2022-01-03 17:13:04 +0000

    🏍🛤 Updated with Glitch - Updated deploy-commands.js file to dynamically search for new commands each time it is ran

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,4 +1,5 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const fs = require('fs');
+//  const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 
@@ -7,11 +8,13 @@ const   guildId   = process.env.GUILD_ID;
 const   clientId  = process.env.CLIENT_ID;
 
 
-const commands = [
-  new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-  new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-  new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-].map(command => command.toJSON());
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles){
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
 
 const rest = new REST({ version: '9' }).setToken(token);
 
